dfe0807bfbf7512435279e846a423a6204b89d5b
Tiago Angelo <kurtis.angelo@gmail.com>
2026-04-19 10:12:44 -0300

    Sixteenth commit

--- internal/provider/github_test.go
+++ internal/provider/github_test.go
@@ -1,16 +1,13 @@
 package provider
 
 import (
+	"regexp"
 	"testing"
 
 	"github.com/hashicorp/terraform-plugin-framework/providerserver"
 	"github.com/hashicorp/terraform-plugin-go/tfprotov6"
 	"github.com/hashicorp/terraform-plugin-testing/helper/resource"
 )
-
-const providerConfig = `
-provider "github" {}
-`
 
 var testAccProtoV6ProviderFactories = map[string]func() (tfprotov6.ProviderServer, error){
 	"github": providerserver.NewProtocol6WithError(NewGitHub()),
@@ -20,8 +17,10 @@ func TestGitHub_Configure(t *testing.T) {
 	resource.Test(t, resource.TestCase{
 		ProtoV6ProviderFactories: testAccProtoV6ProviderFactories,
 		Steps: []resource.TestStep{
+			{Config: `provider "github" {}`},
 			{
-				Config: providerConfig,
+				Config:      `data "github_repositories" "_" {}`,
+				ExpectError: regexp.MustCompile(`The argument "token" is required`),
 			},
 		},
 	})
